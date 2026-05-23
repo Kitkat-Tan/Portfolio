d75c1603e5aa0cd5b58c49e248e7e29ee9b6feb1
Katrina Tan Napigkit <katrinatannapigkit@gmail.com>
2026-05-23 17:17:48 +0800

    i

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-function toggleTheme() {
-    document.body.classList.toggle('dark-mode');
-    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
-}
 
-if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-mode');
-}
 
 window.addEventListener('load', () => {
     setTimeout(() => {
